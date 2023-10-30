60d5e294e485ed33ab0a9d31004ae77303edfa6c
RedMaster974 <paulkbidi57@gmail.com>
2023-10-30 15:39:25 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const fetchButton = document.getElementById("blagues");
+var categories = document.querySelector('select');
+categories.addEventListener('change', function(){
+    const api = `https://api.blablagues.net/?rub=blagues&cat=${categories.value}`;
+    fetchButton.addEventListener("click", async () => {
+      await fetch(api)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          const text = data.data.content.text;
+          const text_head = data.data.content.text_head;
+          const text_hidden = data.data.content.text_hidden;
+
+          const textElement = document.getElementById("text");
+          const textHeadElement = document.getElementById("text_head");
+          const textHiddenElement = document.getElementById("text_hidden");
+
+          textElement.textContent = `${text}`;
+          textHeadElement.textContent = `${text_head}`;
+          textHiddenElement.textContent = `${text_hidden}`;
+        });
+    });
+});
